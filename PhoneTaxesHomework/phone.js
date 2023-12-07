a51e76bd5b1b96a24c7939d6daa29ab9ad038fea
Marko-Gulevski <markogulevski16@gmail.com>
2023-12-08 00:35:46 +0100

    Domashna so Odanocheni Telefoni

---
+++ PhoneTaxesHomework/phone.js
@@ -0,0 +1,17 @@
+let phonePrice = 119.95;
+let phoneAmount = 30;
+let taxRate = 0.05;
+let phonePriceWithTax = phonePrice + (phonePrice * taxRate);
+let totalPriceAllPhones = phoneAmount * phonePriceWithTax;
+console.log(totalPriceAllPhones);
+
+
+/*
+Alternate Method
+ let phonePrice = 119.95;
+// let phoneAmount = 30;
+// let taxRate = 0.05;
+// let phonePricesNoTax = phonePrice * phoneAmount;
+// let finalPrice = phonePricesNoTax + (phonePricesNoTax * taxRate);
+// console.log(finalPrice);
+*/
